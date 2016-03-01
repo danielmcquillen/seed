ce520770a3f4c6cb15d665ca5cbf9aed9546dcdb
danielmcquillen <daniel@mcquilleninteractive.com>
2016-03-02 10:02:31 +1100

    Changing up a bit how the clear db shell script works.

--- bin/drop_create_db.sh
+++ bin/drop_create_db.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 # drops the ``seed`` DB, then creates it. Add a super_user
-# demo@buidlingenergy.com with password demo
 
-dropdb seed
-createdb seed
-python manage.py syncdb --migrate
-python manage.py create_default_user
+echo "Dropping db..."
+sudo -H -u postgres bash -c 'dropdb seed-deploy'
+
+echo "Creating db..."
+sudo -H -u postgres bash -c 'createdb seed-deploy'
+
+echo "Granting privileges..."
+sudo -H -u postgres bash -c 'psql -c "GRANT ALL PRIVILEGES ON DATABASE \"seed-deploy\" to dev_db_user;"'
+
+echo "Move to seed folder"
+cd ~/seed
+
+echo "Migrating db..."
+python manage.py migrate
+
+echo "Creating default user"
+python manage.py create_default_user --username=$SEED_DB_USERNAME --password=$SEED_DB_PASS
+
